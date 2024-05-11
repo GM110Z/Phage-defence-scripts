0d3edc37af63b1d0c3ac12e40898d1be59e5dac5
GM110Z <72254136+GM110Z@users.noreply.github.com>
2024-05-11 16:03:12 +0100

    Create gbktofaa.py

---
+++ PADLOC-batch-runs/gbktofaa.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+from Bio import SeqIO
+import sys
+file_name = sys.argv[1]
+
+# stores all the CDS entries
+all_entries = []
+
+with open(file_name, 'r') as GBFile:
+
+    GBcds = SeqIO.InsdcIO.GenBankCdsFeatureIterator(GBFile)
+
+    for cds in GBcds:
+        if cds.seq is not None:
+            cds.id = cds.id+"|"+ cds.name+"|"+cds.description       #can also just use cds.id
+            cds.description = ''
+            all_entries.append(cds)
+
+
+# write file
+SeqIO.write(all_entries, '{}.fasta'.format(file_name[:-3]), 'fasta')
