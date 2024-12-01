33eb888fc3c2e765890c44c83655dee56f36e837
GM110Z <72254136+GM110Z@users.noreply.github.com>
2024-12-01 09:52:16 +0000

    Update padloc-preprocessing.sh

--- PADLOC-batch-runs/padloc-preprocessing.sh
+++ PADLOC-batch-runs/padloc-preprocessing.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #convert gb to gff3
-bp_genbank2gff3 *.gb
+VAR4=$(echo *.gb | xargs ls)
 
-echo 'cleaning gff files'
-#clean gff for padloc
-VAR4=$(echo *.gff | xargs ls)
+for f in ${VAR4}
+   do
+      python gbktogff.py  ${f}
+   done
 
-for file in ${VAR4}
-
-        do
-           sed '/^##FASTA/Q' ${file} >${file}_noseq.gff
-        done
 
 #conver gb to protein fasta
 VAR4=$(echo *.gb | xargs ls)
